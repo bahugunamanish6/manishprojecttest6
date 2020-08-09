3390bc9e9d5b42d3c4a2d90cd069b4ca7b4b5a56
Dell <Dell@DESKTOP-0K01G74>
2020-08-09 12:29:13 +0530

    commit

---
+++ Selenium webdriver/src/pkg2/FacebookSignup.java
@@ -0,0 +1,37 @@
+package pkg2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FacebookSignup 
+{
+	public static void main(String[] args) 
+	{
+        System.setProperty("webdriver.chrome.driver", "../Selenium webdriver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.manage().window().maximize();
+        
+        
+        WebElement firstname=driver.findElement(By.name("firstname"));
+		firstname.sendKeys("Manish");
+		
+		WebElement lastname=driver.findElement(By.name("lastname"));
+		lastname.sendKeys("Bahuguna");
+		
+		WebElement email=driver.findElement(By.cssSelector("input[name= 'reg_email__'"));
+		email.sendKeys("manishbahuguna6@gmail.com");
+		
+		WebElement pass=driver.findElement(By.id("password_step_input"));
+		pass.sendKeys("456@dkgfhg55");
+		
+		WebElement SignUp=driver.findElement(By.name("wesubmit"));
+		
+		SignUp.click();
+		
+		driver.close();
+	}
+	
+
+}
